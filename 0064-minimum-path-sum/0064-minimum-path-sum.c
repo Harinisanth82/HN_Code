04a6491b4e32ef82c41707f7c485fd1054187c0d
Hari Nisanth T M <151635996+Harinisanth82@users.noreply.github.com>
2025-02-15 21:57:50 +0530

    Time: 0 ms (100%), Space: 10.6 MB (57.65%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.c
@@ -0,0 +1,18 @@
+int minPathSum(int** grid, int gridSize, int* gridColSize) {
+    int r=gridSize;
+    int c=*gridColSize;
+    int dp[r][c],i,j;
+    dp[0][0]=grid[0][0];
+    for(i=1;i<r;i++){
+        dp[i][0]=grid[i][0]+dp[i-1][0];
+    }
+    for(i=1;i<c;i++){
+        dp[0][i]=grid[0][i]+dp[0][i-1];
+    }
+    for(i=1;i<r;i++){
+        for(j=1;j<c;j++){
+            dp[i][j]=grid[i][j]+(dp[i-1][j]<dp[i][j-1] ? dp[i-1][j]:dp[i][j-1]);
+        }
+    }
+    return dp[r-1][c-1];
+}
