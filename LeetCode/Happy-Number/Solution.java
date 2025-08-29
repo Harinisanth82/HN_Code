f6ea3b861c2cd7c5b71f8f222528db6ca24e76c0
Hari Nisanth T M <151635996+Harinisanth82@users.noreply.github.com>
2025-08-29 21:43:45 +0530

    Add solution for Happy Number

--- LeetCode/Happy-Number/Solution.java
+++ LeetCode/Happy-Number/Solution.java
@@ -1,16 +1,20 @@
-class Solution:
-    def isHappy(self, n: int) -> bool:
-        ans=set()
-        while n not in ans:
-            ans.add(n)
-            cpy=0
-            inp=n
-            digit=0
-            while inp:
-                cpy=int(inp%10)
-                digit+=cpy**2
-                inp=int(inp/10)
-            n=digit
-            if n==1:
-                return True
-        return False
+class Solution {
+    public int compute(int num){
+        int res = 0;
+        while(num != 0){
+            res = res + ((num%10)*(num%10));
+            num = num/10;
+        }
+        return res;
+    }
+    public boolean isHappy(int n) {
+        int slow,fast;
+        slow=fast=n;
+        do{
+            slow = compute(slow);
+            fast = compute(compute(fast));
+        }while(slow != fast);
+        
+        return slow==1;
+    }
+}
