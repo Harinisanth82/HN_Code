89c538a8ea797a62fa207b7fd36af84a5cbdffde
Hari Nisanth T M <151635996+Harinisanth82@users.noreply.github.com>
2026-03-07 10:05:42 +0530

    Add solution for Valid Parentheses

--- LeetCode/Valid-Parentheses/Solution.java
+++ LeetCode/Valid-Parentheses/Solution.java
@@ -1,25 +1,25 @@
-class Solution {
-    public boolean isValid(String s) {
-      Stack<Character> game = new Stack<>();
-      for(char play : s.toCharArray()){
-        if(play=='(' || play=='{' || play=='['){
-            game.push(play);
-        }
-        else{
-            if(game.isEmpty()){
-                return false;
-            }
-            if(play==')' && game.pop()!='('){
-                return false;
-            }
-            if(play=='}' && game.pop()!='{'){
-                return false;
-            }
-            if(play==']' && game.pop()!='['){
-                return false;
-            }
-        }
-      }
-      return game.isEmpty();  
-    }
-}
+1class Solution {
+2    public boolean isValid(String s) {
+3      Stack<Character> game = new Stack<>();
+4      for(char play : s.toCharArray()){
+5        if(play=='(' || play=='{' || play=='['){
+6            game.push(play);
+7        }
+8        else{
+9            if(game.isEmpty()){
+10                return false;
+11            }
+12            if(play==')' && game.pop()!='('){
+13                return false;
+14            }
+15            if(play=='}' && game.pop()!='{'){
+16                return false;
+17            }
+18            if(play==']' && game.pop()!='['){
+19                return false;
+20            }
+21        }
+22      }
+23      return game.isEmpty();  
+24    }
+25}
