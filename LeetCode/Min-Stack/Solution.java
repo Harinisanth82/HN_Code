bc3aa3191db597ee297e4864ab875b75e3268993
Hari Nisanth T M <151635996+Harinisanth82@users.noreply.github.com>
2026-03-09 10:07:26 +0530

    Add solution for Min Stack

--- LeetCode/Min-Stack/Solution.java
+++ LeetCode/Min-Stack/Solution.java
@@ -1,28 +1,28 @@
 1class MinStack {
-2    Stack<int[]> s;
+2    Stack<int[]> st;
 3    public MinStack() {
-4        s = new Stack();      
+4        st = new Stack();
 5    }
 6    
 7    public void push(int val) {
-8        if(s.isEmpty()){
-9            s.push(new int[]{val,val});
+8        if(st.isEmpty()){
+9            st.push(new int[]{val,val});
 10            return;
 11        }
-12        int minimum = s.peek()[1];
-13        s.push(new int[]{val,Math.min(val,minimum)});
+12        int min = st.peek()[1];
+13        st.push(new int[]{val,Math.min(val,min)});
 14    }
 15    
 16    public void pop() {
-17        s.pop();
+17        st.pop();
 18    }
 19    
 20    public int top() {
-21        return s.peek()[0];
+21        return st.peek()[0];
 22    }
 23    
 24    public int getMin() {
-25        return s.peek()[1];
+25        return st.peek()[1];   
 26    }
 27}
 28
