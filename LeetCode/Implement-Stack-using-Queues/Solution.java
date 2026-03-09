d8ccbb502398dcff8bb332c1bccadab98e916453
Hari Nisanth T M <151635996+Harinisanth82@users.noreply.github.com>
2026-03-09 09:34:00 +0530

    Add solution for Implement Stack using Queues

--- LeetCode/Implement-Stack-using-Queues/Solution.java
+++ LeetCode/Implement-Stack-using-Queues/Solution.java
@@ -1,31 +1,31 @@
-class MyStack {
-    Deque<Integer> q;
-    public MyStack() {
-        q = new ArrayDeque();
-    }
-    
-    public void push(int x) {
-        q.addLast(x);
-    }
-    
-    public int pop() {
-        return q.removeLast();
-    }
-    
-    public int top() {
-        return q.peekLast();
-    }
-    
-    public boolean empty() {
-        return q.isEmpty();
-    }
-}
-
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack obj = new MyStack();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.top();
- * boolean param_4 = obj.empty();
- */
+1class MyStack {
+2    Deque<Integer> q;
+3    public MyStack() {
+4        q = new ArrayDeque();
+5    }
+6    
+7    public void push(int x) {
+8        q.addLast(x);
+9    }
+10    
+11    public int pop() {
+12        return q.removeLast();
+13    }
+14    
+15    public int top() {
+16        return q.peekLast();
+17    }
+18    
+19    public boolean empty() {
+20        return q.isEmpty();
+21    }
+22}
+23
+24/**
+25 * Your MyStack object will be instantiated and called as such:
+26 * MyStack obj = new MyStack();
+27 * obj.push(x);
+28 * int param_2 = obj.pop();
+29 * int param_3 = obj.top();
+30 * boolean param_4 = obj.empty();
+31 */
