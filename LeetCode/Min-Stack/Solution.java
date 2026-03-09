1d75dc5c35568c3c6f59e13eb7dd689b7180c31e
Hari Nisanth T M <151635996+Harinisanth82@users.noreply.github.com>
2026-03-09 09:55:33 +0530

    Add solution for Min Stack

--- LeetCode/Min-Stack/Solution.java
+++ LeetCode/Min-Stack/Solution.java
@@ -1,36 +1,36 @@
-class MinStack {
-    Stack<int[]> s;
-    public MinStack() {
-        s = new Stack();      
-    }
-    
-    public void push(int val) {
-        if(s.isEmpty()){
-            s.push(new int[]{val,val});
-            return;
-        }
-        int minimum = s.peek()[1];
-        s.push(new int[]{val,Math.min(val,minimum)});
-    }
-    
-    public void pop() {
-        s.pop();
-    }
-    
-    public int top() {
-        return s.peek()[0];
-    }
-    
-    public int getMin() {
-        return s.peek()[1];
-    }
-}
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
+1class MinStack {
+2    Stack<int[]> s;
+3    public MinStack() {
+4        s = new Stack();      
+5    }
+6    
+7    public void push(int val) {
+8        if(s.isEmpty()){
+9            s.push(new int[]{val,val});
+10            return;
+11        }
+12        int minimum = s.peek()[1];
+13        s.push(new int[]{val,Math.min(val,minimum)});
+14    }
+15    
+16    public void pop() {
+17        s.pop();
+18    }
+19    
+20    public int top() {
+21        return s.peek()[0];
+22    }
+23    
+24    public int getMin() {
+25        return s.peek()[1];
+26    }
+27}
+28
+29/**
+30 * Your MinStack object will be instantiated and called as such:
+31 * MinStack obj = new MinStack();
+32 * obj.push(val);
+33 * obj.pop();
+34 * int param_3 = obj.top();
+35 * int param_4 = obj.getMin();
+36 */
