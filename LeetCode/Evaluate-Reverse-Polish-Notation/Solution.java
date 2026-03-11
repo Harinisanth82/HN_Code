9009c44f0f3ff2a55b100497be85f86e791299d4
Hari Nisanth T M <151635996+Harinisanth82@users.noreply.github.com>
2026-03-11 11:46:07 +0530

    Add solution for Evaluate Reverse Polish Notation

--- LeetCode/Evaluate-Reverse-Polish-Notation/Solution.java
+++ LeetCode/Evaluate-Reverse-Polish-Notation/Solution.java
@@ -1,30 +1,30 @@
-class Solution {
-    public int evalRPN(String[] t) {
-        int opt_1,opt_2;
-        Stack<Integer> st = new Stack<>();
-        for(String ch : t){
-            switch(ch){
-                case "+":
-                    st.push(st.pop()+st.pop());
-                    break;
-                case "-":
-                    opt_1 = st.pop();
-                    opt_2 = st.pop();
-                    st.push(opt_2 - opt_1);
-                    break;
-                case "*":
-                    st.push(st.pop() * st.pop());
-                    break;
-                case "/":
-                    opt_1 = st.pop();
-                    opt_2 = st.pop();
-                    st.push((int)(opt_2 / opt_1));
-                    break;
-                default:
-                    st.push(Integer.parseInt(ch));
-                    break;
-            }
-        }
-        return st.size() > 0 ? st.peek() : 0;
-    }
-}
+1class Solution {
+2    public int evalRPN(String[] t) {
+3        int opt_1,opt_2;
+4        Stack<Integer> st = new Stack<>();
+5        for(String ch : t){
+6            switch(ch){
+7                case "+":
+8                    st.push(st.pop()+st.pop());
+9                    break;
+10                case "-":
+11                    opt_1 = st.pop();
+12                    opt_2 = st.pop();
+13                    st.push(opt_2 - opt_1);
+14                    break;
+15                case "*":
+16                    st.push(st.pop() * st.pop());
+17                    break;
+18                case "/":
+19                    opt_1 = st.pop();
+20                    opt_2 = st.pop();
+21                    st.push((int)(opt_2 / opt_1));
+22                    break;
+23                default:
+24                    st.push(Integer.parseInt(ch));
+25                    break;
+26            }
+27        }
+28        return st.size() > 0 ? st.peek() : 0;
+29    }
+30}
