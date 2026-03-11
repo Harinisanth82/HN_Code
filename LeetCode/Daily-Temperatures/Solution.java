37757c626dccf500d7bdaa300eb570f0ab22dce9
Hari Nisanth T M <151635996+Harinisanth82@users.noreply.github.com>
2026-03-11 10:55:58 +0530

    Add solution for Daily Temperatures

--- LeetCode/Daily-Temperatures/Solution.java
+++ LeetCode/Daily-Temperatures/Solution.java
@@ -1,21 +1,21 @@
-class Solution {
-    public int[] dailyTemperatures(int[] t) {
-        int lgt = t.length;
-        int[] res = new int[lgt];
-        Stack<Integer> st = new Stack<>();
-        for(int i = lgt-1;i >= 0; i--){
-            int digit = t[i];
-            while(!st.isEmpty() && digit >= t[st.peek()]){
-                st.pop();
-            }
-            if(st.isEmpty()){
-                res[i] = 0;
-            }
-            else{
-                res[i] = st.peek()-i;
-            }
-            st.push(i);
-        }
-        return res;
-    }
-}
+1class Solution {
+2    public int[] dailyTemperatures(int[] t) {
+3        int lgt = t.length;
+4        int[] res = new int[lgt];
+5        Stack<Integer> st = new Stack<>();
+6        for(int i = lgt-1;i >= 0; i--){
+7            int digit = t[i];
+8            while(!st.isEmpty() && digit >= t[st.peek()]){
+9                st.pop();
+10            }
+11            if(st.isEmpty()){
+12                res[i] = 0;
+13            }
+14            else{
+15                res[i] = st.peek()-i;
+16            }
+17            st.push(i);
+18        }
+19        return res;
+20    }
+21}
