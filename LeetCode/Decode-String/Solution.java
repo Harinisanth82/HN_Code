c9709fe4467d3de255fbf7530dbd8c5f09c90ad3
Hari Nisanth T M <151635996+Harinisanth82@users.noreply.github.com>
2026-03-11 11:56:17 +0530

    Add solution for Decode String

--- LeetCode/Decode-String/Solution.java
+++ LeetCode/Decode-String/Solution.java
@@ -1,36 +1,36 @@
-class Solution {
-    public String decodeString(String s) {
-        int num = 0,times = 0;
-        String prev = "";
-        String curr = "";
-
-        Stack<Integer> st = new Stack<>();
-        Stack<String> ct = new Stack<>();
-        
-        for(char ch : s.toCharArray()){
-            if(Character.isDigit(ch)){
-                num = (num * 10) + (ch - '0');
-            }
-            else if(ch == '['){
-                st.push(num);
-                ct.push(curr);
-
-                num = 0;
-                curr = "";
-            }
-            else if(ch == ']'){
-                times = st.pop();
-                prev = ct.pop();
-                StringBuilder temp = new StringBuilder(prev);
-                for(int i = 1; i <= times; i++){
-                    temp.append(curr);
-                }
-                curr = temp.toString();
-            }
-            else{
-                curr += ch;
-            }
-        }
-        return curr;
-    }
-}
+1class Solution {
+2    public String decodeString(String s) {
+3        int num = 0,times = 0;
+4        String prev = "";
+5        String curr = "";
+6
+7        Stack<Integer> st = new Stack<>();
+8        Stack<String> ct = new Stack<>();
+9        
+10        for(char ch : s.toCharArray()){
+11            if(Character.isDigit(ch)){
+12                num = (num * 10) + (ch - '0');
+13            }
+14            else if(ch == '['){
+15                st.push(num);
+16                ct.push(curr);
+17
+18                num = 0;
+19                curr = "";
+20            }
+21            else if(ch == ']'){
+22                times = st.pop();
+23                prev = ct.pop();
+24                StringBuilder temp = new StringBuilder(prev);
+25                for(int i = 1; i <= times; i++){
+26                    temp.append(curr);
+27                }
+28                curr = temp.toString();
+29            }
+30            else{
+31                curr += ch;
+32            }
+33        }
+34        return curr;
+35    }
+36}
