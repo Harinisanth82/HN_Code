81ba0a43a7fdbdd1c3b3ba0497c3dbb694c0ba4a
Hari Nisanth T M <151635996+Harinisanth82@users.noreply.github.com>
2026-03-11 10:16:43 +0530

    Add solution for Next Greater Element I

--- LeetCode/Next-Greater-Element-I/Solution.java
+++ LeetCode/Next-Greater-Element-I/Solution.java
@@ -1,23 +1,23 @@
 1class Solution {
 2    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-3        Map<Integer,Integer> map = new HashMap<>();
-4        Stack<Integer> st = new Stack<>();
-5        for(int i = nums2.length-1;i >= 0; i--){
+3        Map<Integer,Integer> mp = new HashMap();
+4        Stack<Integer> st = new Stack();
+5        for(int i=nums2.length-1;i>=0;i--){
 6            int inp = nums2[i];
 7            while(!st.isEmpty() && st.peek()<=inp){
 8                st.pop();
 9            }
 10            if(st.isEmpty()){
-11                map.put(inp,-1);
+11                mp.put(inp,-1);
 12            }
 13            else{
-14                map.put(inp,st.peek());
+14                mp.put(inp,st.peek());
 15            }
 16            st.push(inp);
 17        }
 18        int[] result = new int[nums1.length];
-19        for (int i = 0; i < nums1.length; i++) {
-20            result[i] = map.get(nums1[i]);
+19        for(int i=0;i<nums1.length;i++){
+20            result[i] = mp.get(nums1[i]);
 21        }
 22        return result;
 23    }
